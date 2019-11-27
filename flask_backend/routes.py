ed0bad19eea59f2be659f9184671d74116050512
Moritz Makowski <makowskimoritz@gmail.com>
2019-11-27 18:50:31 +0100

    Added routes for react and backend status

--- flask_backend/routes.py
+++ flask_backend/routes.py
@@ -1,5 +1,13 @@
 from flask_backend import app
+from flask import render_template
 
-@app.route("/", methods=["GET"])
+
+@app.route("/backend", methods=["GET"])
 def index():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Welcome to Flask!</h1>"
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # your processing here
+    return render_template("index.html")
