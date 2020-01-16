754ca9f0804e5e877e8dff771bf056da2245fba7
Moritz Makowski <makowskimoritz@gmail.com>
2020-01-16 09:50:21 +0100

    Added new production build (gbr country host)

---
+++ flask_backend/static/react/precache-manifest.f7350e91c00fe662b1abbb66f1c76772.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96085ebbfb92f712d5894b91ecaf0377",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "3953d99307a8c9c0edac",
+    "url": "/static/react/css/10.7a9f5ff1.chunk.css"
+  },
+  {
+    "revision": "20913389338cab4f50b5",
+    "url": "/static/react/css/11.9812ad71.chunk.css"
+  },
+  {
+    "revision": "fdf51b2faa47eb2ffa65",
+    "url": "/static/react/css/12.1082c567.chunk.css"
+  },
+  {
+    "revision": "d4ed6fed1e06cb185679",
+    "url": "/static/react/css/13.fbcbcf26.chunk.css"
+  },
+  {
+    "revision": "1d5a18a138290ea6be54",
+    "url": "/static/react/css/14.54e1616b.chunk.css"
+  },
+  {
+    "revision": "862062e34a045c8bd235",
+    "url": "/static/react/css/6.e9f0eaf9.chunk.css"
+  },
+  {
+    "revision": "2431cb00795956f49aa4",
+    "url": "/static/react/css/7.81a57c1d.chunk.css"
+  },
+  {
+    "revision": "a5e0a5a03891f64db49c",
+    "url": "/static/react/css/8.27840bae.chunk.css"
+  },
+  {
+    "revision": "110da9d0654e1c495ae0",
+    "url": "/static/react/css/9.3fae9700.chunk.css"
+  },
+  {
+    "revision": "4dac3201c16759033245",
+    "url": "/static/react/css/main.fddb9f07.chunk.css"
+  },
+  {
+    "revision": "e75776fec1a111c6d95e",
+    "url": "/static/react/js/0.7a2426e5.chunk.js"
+  },
+  {
+    "revision": "9c22f6d3f93eb39b4ff0",
+    "url": "/static/react/js/1.9e7ebbb3.chunk.js"
+  },
+  {
+    "revision": "3953d99307a8c9c0edac",
+    "url": "/static/react/js/10.2e1750cb.chunk.js"
+  },
+  {
+    "revision": "20913389338cab4f50b5",
+    "url": "/static/react/js/11.4d458d56.chunk.js"
+  },
+  {
+    "revision": "fdf51b2faa47eb2ffa65",
+    "url": "/static/react/js/12.6ce01822.chunk.js"
+  },
+  {
+    "revision": "d4ed6fed1e06cb185679",
+    "url": "/static/react/js/13.44356ec7.chunk.js"
+  },
+  {
+    "revision": "1d5a18a138290ea6be54",
+    "url": "/static/react/js/14.0dff634b.chunk.js"
+  },
+  {
+    "revision": "3d442b936600a53d641a",
+    "url": "/static/react/js/15.4865c9bb.chunk.js"
+  },
+  {
+    "revision": "ec288005c58887c7e0eb",
+    "url": "/static/react/js/4.9239ff77.chunk.js"
+  },
+  {
+    "revision": "bddf770a7f14bdfb10ec",
+    "url": "/static/react/js/5.4b7f4e76.chunk.js"
+  },
+  {
+    "revision": "862062e34a045c8bd235",
+    "url": "/static/react/js/6.f8aacc6f.chunk.js"
+  },
+  {
+    "revision": "2431cb00795956f49aa4",
+    "url": "/static/react/js/7.e4ca0ab8.chunk.js"
+  },
+  {
+    "revision": "a5e0a5a03891f64db49c",
+    "url": "/static/react/js/8.2038c5b0.chunk.js"
+  },
+  {
+    "revision": "110da9d0654e1c495ae0",
+    "url": "/static/react/js/9.9aa23145.chunk.js"
+  },
+  {
+    "revision": "4dac3201c16759033245",
+    "url": "/static/react/js/main.6e3e9059.chunk.js"
+  },
+  {
+    "revision": "00016a260b72efadfbf6",
+    "url": "/static/react/js/runtime-main.afa32e83.js"
+  },
+  {
+    "revision": "86d6d9feb54dbf6b7a82648fa5a622c6",
+    "url": "/static/react/media/2305_Landscape_1200.86d6d9fe.jpg"
+  },
+  {
+    "revision": "c3dc00681050ff0174e84e09b0d7a048",
+    "url": "/static/react/media/2305_Landscape_2000.c3dc0068.jpg"
+  },
+  {
+    "revision": "39165ec843600ae37a9186f80a9f18c5",
+    "url": "/static/react/media/2305_Landscape_2800.39165ec8.jpg"
+  },
+  {
+    "revision": "2007038ccbe301747e7d8ad3d3e592d9",
+    "url": "/static/react/media/2305_Portrait_1600.2007038c.jpg"
+  },
+  {
+    "revision": "7f3cd95219375a834bc872e8093c296e",
+    "url": "/static/react/media/2305_Portrait_2200.7f3cd952.jpg"
+  },
+  {
+    "revision": "5396f66ec42c48b834dd3f935fce4194",
+    "url": "/static/react/media/2305_Portrait_2800.5396f66e.jpg"
+  },
+  {
+    "revision": "f5537fe5d9874069b0c00b35a62eea82",
+    "url": "/static/react/media/2305_Square_1200.f5537fe5.jpg"
+  },
+  {
+    "revision": "59f5a43a2a9e4ce312648f0fcd80f256",
+    "url": "/static/react/media/2305_Square_2000.59f5a43a.jpg"
+  },
+  {
+    "revision": "47256b06bc2d72901ac15a1dd57cccfa",
+    "url": "/static/react/media/2305_Square_2800.47256b06.jpg"
+  },
+  {
+    "revision": "8e38ee9ae003c142023a7fd35efe4b8a",
+    "url": "/static/react/media/9718_Landscape_1200.8e38ee9a.jpg"
+  },
+  {
+    "revision": "dc0875a71befc7f3bde7827ff15fca68",
+    "url": "/static/react/media/9718_Landscape_2000.dc0875a7.jpg"
+  },
+  {
+    "revision": "2cfdf75bcb436d9b83a1be64d36a4bb9",
+    "url": "/static/react/media/9718_Landscape_2800.2cfdf75b.jpg"
+  },
+  {
+    "revision": "e16ed021149cba7a2fee8aabe0a56edf",
+    "url": "/static/react/media/9718_Square_1200.e16ed021.jpg"
+  },
+  {
+    "revision": "f74e5f88a061b3fbf7580fd1a47f3cf9",
+    "url": "/static/react/media/9718_Square_2000.f74e5f88.jpg"
+  },
+  {
+    "revision": "da527df714fab84ff5481edd9e7e7ba0",
+    "url": "/static/react/media/9718_Square_2800.da527df7.jpg"
+  },
+  {
+    "revision": "00cddc0c752d7defa952027b7852903f",
+    "url": "/static/react/media/AUSFlag.00cddc0c.png"
+  },
+  {
+    "revision": "50f3c85dd7f00627e6a229476490a454",
+    "url": "/static/react/media/CANFlag.50f3c85d.svg"
+  },
+  {
+    "revision": "c31a7d3c9e972dfd23f58223d37e4e4c",
+    "url": "/static/react/media/EURFlag.c31a7d3c.svg"
+  },
+  {
+    "revision": "8d241d3f84b5e82c0616fbdf14ce9ae8",
+    "url": "/static/react/media/EventLogo.8d241d3f.svg"
+  },
+  {
+    "revision": "a4c61e06c2705c9b45f3c7ab0e4c088e",
+    "url": "/static/react/media/EventLogo.a4c61e06.svg"
+  },
+  {
+    "revision": "03221ca7a38760e285a888bbabc980c5",
+    "url": "/static/react/media/HeaderLogo.03221ca7.svg"
+  },
+  {
+    "revision": "bc44cdcb2f7b09be995bf8a1e5ed72fe",
+    "url": "/static/react/media/JPNFlag.bc44cdcb.svg"
+  },
+  {
+    "revision": "4872af729730643e77afac307e0d8b8f",
+    "url": "/static/react/media/Marina_Landscape_1200.4872af72.jpg"
+  },
+  {
+    "revision": "b6e81b72f79b14d776023c8913f720fb",
+    "url": "/static/react/media/Marina_Landscape_2000.b6e81b72.jpg"
+  },
+  {
+    "revision": "fa85b018caa5ab04648affd304ed0085",
+    "url": "/static/react/media/Marina_Landscape_2800.fa85b018.jpg"
+  },
+  {
+    "revision": "454918036086f2836f39281c8159d230",
+    "url": "/static/react/media/Marina_Square_1200.45491803.jpg"
+  },
+  {
+    "revision": "bac4c5fca4bad6d9ae738b69da3e3819",
+    "url": "/static/react/media/Marina_Square_2000.bac4c5fc.jpg"
+  },
+  {
+    "revision": "907c107bb89d6c48518ec08da98b8483",
+    "url": "/static/react/media/Marina_Square_2800.907c107b.jpg"
+  },
+  {
+    "revision": "662539e7fd61f46cd9acebd1815fcbd6",
+    "url": "/static/react/media/USAFlag.662539e7.svg"
+  },
+  {
+    "revision": "46215b9602e2063c9b3d7ac86c52de0b",
+    "url": "/static/react/media/vr_sport_logo.46215b96.svg"
+  }
+]);
--- flask_backend/static/react/service-worker.js
+++ flask_backend/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.b69eb953aa1efbe18c242663dc2ded00.js"
+  "/static/react/precache-manifest.f7350e91c00fe662b1abbb66f1c76772.js"
 );
 
 self.addEventListener('message', (event) => {
